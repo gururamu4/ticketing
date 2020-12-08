53ca18087f6fc11f192679cd092b63737fba09c5
guru <gururamu4497@gmail.com>
2020-12-08 15:48:34 +0530

    unwanted packages

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,7 +2,6 @@ import express, { json } from "express";
 import 'express-async-errors';
 import { errorHandler } from "./middlewares/errorHandler";
 import { SignupRouter } from "./routes";
-const test  = require("@guru/common-utils");
 
 const app = express();
 
@@ -10,7 +9,6 @@ app.use(json());
 app.use(SignupRouter)
 app.use(errorHandler);
 app.listen("3000", () => {
-    test();
     console.log(`running on sport 3000`)
 });
 
